65d63ff72e5a1c0cc5038af3a817c8af3d55afae
designfausto <design_fausto@outlook.com>
2023-02-04 15:35:18 +0000

    Menu fix

--- assets/js/script_design.js
+++ assets/js/script_design.js
@@ -1,23 +1,6 @@
 
 
 
-// script for about
-var t1 = new TimelineMax({
-    paused: true
-});
-
-t1.to(".menu", 2, {
-    right: "0%",
-    position: "fixed",
-    ease: Expo.easeInOut,
-});
-
-t1.reverse();
-$(document).on("click", ".about-btn", function () {
-    t1.reversed(!t1.reversed());
-});
-
-
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
