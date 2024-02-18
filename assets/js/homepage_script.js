0f38c66de1925dc7d02c8b3538b4b741b616b185
designfausto <design_fausto@outlook.com>
2024-02-18 14:24:44 +0000

    Text and other stuffs

--- assets/js/homepage_script.js
+++ assets/js/homepage_script.js
@@ -1,26 +1,9 @@
+let links = document.querySelectorAll(".links a");
+let bodyId = document.querySelectorAll("body").id;
 
 
-// script for about
-var t1 = new TimelineMax({
-    paused: true
-});
-
-t1.to(".menu", 2, {
-    right: "0%",
-    position: "fixed",
-    ease: Expo.easeInOut,
-});
-
-t1.reverse();
-$(document).on("click", ".about-btn", function () {
-    t1.reversed(!t1.reversed());
-});
-
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+for(let link of links){
+    if(link.CDATA_SECTION_NODE.active == bodyId){
+        link.classList.add("active");
+    }
+}
